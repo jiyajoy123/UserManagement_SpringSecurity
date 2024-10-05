964cfaba79288b68603ecb8805e704b7704071c7
jiya <jiyajoy123@gmail.com>
2024-10-04 22:47:52 -0400

    Encrypt password

--- src/main/java/com/javaprojects/UserManagement/repo/UserRepository.java
+++ src/main/java/com/javaprojects/UserManagement/repo/UserRepository.java
@@ -3,10 +3,13 @@ package com.javaprojects.UserManagement.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
+import com.javaprojects.UserManagement.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<User> findByUsername(String username);
 }
--- src/main/java/com/javaprojects/UserManagement/service/UserService.java
+++ src/main/java/com/javaprojects/UserManagement/service/UserService.java
@@ -4,20 +4,27 @@ import com.javaprojects.UserManagement.config.UserRegistrationDto;
 import com.javaprojects.UserManagement.model.AppUser;
 import com.javaprojects.UserManagement.repo.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private AppUserRepository userRepository;
 
     public void registerNewUser(UserRegistrationDto registrationDto) {
         AppUser user = new AppUser();
         user.setUsername(registrationDto.getUsername());
-        user.setPassword(registrationDto.getPassword()); // You can add password encryption here
+
+        // Encrypt the password before saving
+        String encodedPassword = passwordEncoder.encode(registrationDto.getPassword());
+        user.setPassword(encodedPassword);
+
         user.setRole(registrationDto.getRole());
 
         userRepository.save(user);
     }
+
 }
